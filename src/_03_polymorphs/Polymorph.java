0f35fd0d4b43f16aef7c1ed4a7efec2558ba3d14
HelloThereGeneralKenobi <37271141+HelloThereGeneralKenobi@users.noreply.github.com>
2020-09-20 17:28:30 -0700

    Worked on polymorphs

--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,13 +3,50 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-    int x;
-    int y;
+	
+    private int x;
+    private int y;
+    private int width;
+    private int height;
     
-    Polymorph(int x, int y){
+    Polymorph(int x, int y,int width, int height){
    	 this.x = x;
    	 this.y = y;
+   	 this.width = width;
+   	 this.height = height;
     }
+    
+    int getX() {
+		return x;
+	}
+    
+    int getY() {
+		return y;
+	}
+    
+    void setX(int x) {
+		this.x = x;
+	}
+    
+    void setY(int y) {
+		this.y = y;
+	}
+    
+    int getWidth() {
+		return width;
+	}
+    
+    void setWidth(int width) {
+		this.width = width;
+	}
+    
+    int getHeight() {
+		return height;
+	}
+    
+    void setHeight(int height) {
+		this.height = height;
+	}
     
     public void update(){
    	 
